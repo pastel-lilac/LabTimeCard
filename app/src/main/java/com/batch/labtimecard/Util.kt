756cec71509ae2bb93c8647a94a3e4b23f72c92f
batch <baaaakkrad@gmail.com>
2020-01-13 14:37:39 +0900

    shared preferences yokuwakran

--- app/src/main/java/com/batch/labtimecard/Util.kt
+++ app/src/main/java/com/batch/labtimecard/Util.kt
@@ -1,5 +1,10 @@
 package com.batch.labtimecard
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
+
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 
@@ -9,5 +14,24 @@ fun ImageView.setImage(labName: String) {
         setImageResource(R.drawable.sarimaru)
     } else {
         setImageResource(R.drawable.enako)
+    }
+}
+
+@BindingAdapter("visible")
+fun View.visible(memberName: String) {
+    Log.d("ORENOTAG", "hehee")
+    val myPref = context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE)
+    myPref.registerOnSharedPreferenceChangeListener { pref: SharedPreferences, key: String ->
+        Log.d("ORENOTAG", "registerの中")
+        if (key == "${memberName}isExisting}") {
+            Log.d("ORENOTAG", "true!!：key=${key}, myPrefKey=${memberName}isExisting")
+            val isExisting = pref.getBoolean(key, false)
+            when (isExisting) {
+                true -> visibility = View.GONE
+                false -> visibility = View.VISIBLE
+            }
+        } else {
+            Log.d("ORENOTAG", "false!!：key=${key}, myPrefKey=${memberName}isExisting")
+        }
     }
 }
