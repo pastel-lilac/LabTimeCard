295e5ff039afc25524074f8ecc49322f2270d4b4
batch <baaaakkrad@gmail.com>
2020-01-23 22:15:00 +0900

    fix: implement login function in MemberListViewModel

--- app/src/main/java/com/batch/labtimecard/ui/MemberListActivity.kt
+++ app/src/main/java/com/batch/labtimecard/ui/MemberListActivity.kt
@@ -1,9 +1,7 @@
 package com.batch.labtimecard.ui
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -13,10 +11,6 @@ import com.batch.labtimecard.R
 import com.batch.labtimecard.model.MemberData
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_member_list.*
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.collections.HashMap
-
 
 class MemberListActivity : AppCompatActivity(), MemberListController.ClickListener {
 
@@ -31,8 +25,6 @@ class MemberListActivity : AppCompatActivity(), MemberListController.ClickListen
         observeMembers()
         database = FirebaseDatabase.getInstance()
         viewModel.fetchFromRemote()
-//        readDatabase()
-
         member_list_recycler_view.apply {
             layoutManager = LinearLayoutManager(applicationContext)
             adapter = controller.adapter
@@ -59,36 +51,6 @@ class MemberListActivity : AppCompatActivity(), MemberListController.ClickListen
     }
 
     override fun itemClickListener(item: MemberData) {
-        val pref = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
-        val date = Date(System.currentTimeMillis())
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd")
-        val today = dateFormat.format(date)
-        val insertDateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
-        val time = insertDateFormat.format(date)
-        val name = item.member.name
-        val timeMap: MutableMap<String, Any> = HashMap()
-        val ref  = database.getReference("logs").child(name.toString()).child(today)
-        if (pref.getBoolean("${name}isExisting", false)) { // 選択されたメンバーがログイン中のとき
-            // databaseに保存する子のキー名をlogoutにする
-            timeMap["logoutTime"] = time
-            Toast.makeText(applicationContext, "ログアウトしました\n名前${name} 時刻${time}", Toast.LENGTH_SHORT).show()
-            pref.edit().apply {
-                putBoolean("${name}isExisting", false)
-                commit()
-            }
-            // ログアウトした時間をdatabaseに保存
-            ref.updateChildren(timeMap)
-        } else { // 選択されたメンバーがログイン中じゃないとき
-            // databaseに保存する子のキー名をloginにする
-            timeMap["loginTime"] = time
-            timeMap["logoutTime"] = ""
-            Toast.makeText(applicationContext, "ログインしました\n名前${name} 時刻${time}", Toast.LENGTH_SHORT).show()
-            pref.edit().apply {
-                putBoolean("${name}isExisting", true)
-                commit()
-            }
-            // ログインした時間をdatabaseに保存
-            ref.setValue(timeMap)
-        }
+        viewModel.login(item)
     }
 }
--- app/src/main/java/com/batch/labtimecard/ui/MemberListViewModel.kt
+++ app/src/main/java/com/batch/labtimecard/ui/MemberListViewModel.kt
@@ -1,8 +1,10 @@
 package com.batch.labtimecard.ui
 
+import android.app.Application
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.batch.labtimecard.R
@@ -12,16 +14,18 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.HashMap
 
-class MemberListViewModel: ViewModel() {
+class MemberListViewModel(application: Application): AndroidViewModel(application) {
 
     val TAG = "ORENOTAG"
 
     val members = MutableLiveData<List<MemberData>>()
     private lateinit var database: FirebaseDatabase
+    private val context = getApplication<Application>().applicationContext
 
     fun fetchFromRemote() {
         val memberDataList: MutableList<MemberData> = mutableListOf()
@@ -42,5 +46,37 @@ class MemberListViewModel: ViewModel() {
             override fun onCancelled(error: DatabaseError) {
             }
         })
+    }
+
+    fun login(item: MemberData) {
+        val pref = context.getSharedPreferences(
+            context.getString(R.string.app_name),
+            Context.MODE_PRIVATE
+        )
+        val date = Date(System.currentTimeMillis())
+        val today = SimpleDateFormat("yyyy-MM-dd").format(date)
+        val time = SimpleDateFormat("HH:mm:ss").format(date)
+        val name = item.member.name
+        val timeMap: MutableMap<String, Any> = HashMap()
+        val ref = database.getReference("logs").child(name.toString()).child(today)
+        if (pref.getBoolean("${name}isExisting", false)) {
+            timeMap["logoutTime"] = time
+            Toast.makeText(context, "ログアウトしました\n名前${name} 時刻${time}", Toast.LENGTH_SHORT).show()
+            pref.edit().apply {
+                putBoolean("${name}isExisting", false)
+                commit()
+            }
+            ref.updateChildren(timeMap)
+        } else {
+            timeMap["loginTime"] = time
+            timeMap["logoutTime"] = ""
+            Toast.makeText(context, "ログインしました\n名前${name} 時刻${time}", Toast.LENGTH_SHORT).show()
+            pref.edit().apply {
+                putBoolean("${name}isExisting", true)
+                commit()
+            }
+            // ログインした時間をdatabaseに保存
+            ref.setValue(timeMap)
+        }
     }
 }
