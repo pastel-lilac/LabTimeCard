66cfef8a8d199b55488892adafbd0e0c17513898
batch <baaaakkrad@gmail.com>
2020-01-23 21:10:32 +0900

    fix: can save member key with using MemberData

--- app/src/main/java/com/batch/labtimecard/ui/MemberListActivity.kt
+++ app/src/main/java/com/batch/labtimecard/ui/MemberListActivity.kt
@@ -3,12 +3,14 @@ package com.batch.labtimecard.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.batch.labtimecard.R
 import com.batch.labtimecard.model.Member
+import com.batch.labtimecard.model.MemberData
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_member_list.*
 import java.text.SimpleDateFormat
@@ -23,7 +25,7 @@ class MemberListActivity : AppCompatActivity(), MemberListController.ClickListen
 
     private lateinit var database: FirebaseDatabase
     private val controller by lazy { MemberListController(this) }
-    val memberList: MutableList<Member> = mutableListOf()
+    val memberDataList: MutableList<MemberData> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,14 +56,15 @@ class MemberListActivity : AppCompatActivity(), MemberListController.ClickListen
         val ref = database.getReference("members")
         ref.addValueEventListener(object: ValueEventListener{
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                memberList.clear()
+                memberDataList.clear()
                 dataSnapshot.children.forEach { ds ->
+                    Log.d(TAG, ds.toString())
                     val member = ds.getValue(Member::class.java)
                     if (member != null) {
-                        memberList.add(member)
+                        memberDataList.add(MemberData(ds.key, member))
                     }
                 }
-                controller.setData(memberList)
+                controller.setData(memberDataList)
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -69,23 +72,22 @@ class MemberListActivity : AppCompatActivity(), MemberListController.ClickListen
         })
     }
 
-    override fun itemClickListener(item: Member) {
+    override fun itemClickListener(item: MemberData) {
         val pref = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
         val date = Date(System.currentTimeMillis())
         val dateFormat = SimpleDateFormat("yyyy-MM-dd")
         val today = dateFormat.format(date)
         val insertDateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
         val time = insertDateFormat.format(date)
-
-
+        val name = item.member.name
         val timeMap: MutableMap<String, Any> = HashMap()
-        val ref  = database.getReference("logs").child(item.name.toString()).child(today)
-        if (pref.getBoolean("${item.name}isExisting", false)) { // 選択されたメンバーがログイン中のとき
+        val ref  = database.getReference("logs").child(name.toString()).child(today)
+        if (pref.getBoolean("${name}isExisting", false)) { // 選択されたメンバーがログイン中のとき
             // databaseに保存する子のキー名をlogoutにする
             timeMap["logoutTime"] = time
-            Toast.makeText(applicationContext, "ログアウトしました\n名前${item.name} 時刻${time}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "ログアウトしました\n名前${name} 時刻${time}", Toast.LENGTH_SHORT).show()
             pref.edit().apply {
-                putBoolean("${item.name}isExisting", false)
+                putBoolean("${name}isExisting", false)
                 commit()
             }
             // ログアウトした時間をdatabaseに保存
@@ -94,9 +96,9 @@ class MemberListActivity : AppCompatActivity(), MemberListController.ClickListen
             // databaseに保存する子のキー名をloginにする
             timeMap["loginTime"] = time
             timeMap["logoutTime"] = ""
-            Toast.makeText(applicationContext, "ログインしました\n名前${item.name} 時刻${time}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "ログインしました\n名前${name} 時刻${time}", Toast.LENGTH_SHORT).show()
             pref.edit().apply {
-                putBoolean("${item.name}isExisting", true)
+                putBoolean("${name}isExisting", true)
                 commit()
             }
             // ログインした時間をdatabaseに保存
