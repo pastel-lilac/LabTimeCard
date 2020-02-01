1daad77acbc741e2a495a5597ba24d3e9e9545e7
gericass <bonjovi1926@yahoo.co.jp>
2020-02-01 20:27:48 +0900

    apply repository pattern

--- app/src/main/java/com/batch/labtimecard/di/Modules.kt
+++ app/src/main/java/com/batch/labtimecard/di/Modules.kt
@@ -1,11 +1,30 @@
 package com.batch.labtimecard.di
 
 import com.batch.labtimecard.common.navigator.Navigator
+import com.batch.labtimecard.data.TimeCardRepository
+import com.batch.labtimecard.data.TimeCardRepositoryImpl
+import com.batch.labtimecard.member.MemberListViewModel
+import com.batch.labtimecard.member.usecase.LoginUseCase
+import com.batch.labtimecard.member.usecase.LoginUseCaseImpl
 import com.batch.labtimecard.navigator.NavigatorImpl
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 object Modules {
     val navigatorModule = module {
         single<Navigator> { NavigatorImpl() }
     }
+
+    val repositoryModule = module {
+        single<TimeCardRepository> { TimeCardRepositoryImpl() }
+    }
+
+    val useCaseModule = module {
+        single<LoginUseCase> { LoginUseCaseImpl(get()) }
+    }
+
+    val viewModelModule = module {
+        viewModel { MemberListViewModel(get(), get()) }
+    }
+
 }
